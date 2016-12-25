319821116b184a1b69ec93d9e922e09202bdbf4e
aleeas <ahmadsuhaili244@gmail.com>
2016-12-25 12:39:20 +0700

    add icon mahasiswa dashboard

--- application/views/header/menu-nav-mahasiswa.php
+++ application/views/header/menu-nav-mahasiswa.php
@@ -1,8 +1,15 @@
 <div id="nav">
       <ul>
         <li><a href="<?php echo site_url("auth/userpanel");?>" class="<?php if($this->uri->uri_string() == 'auth/userpanel') { echo 'active'; } ?>">Home</a></li>
-        <li><a href="<?php echo site_url("mahasiswa/krs");?>" class="<?php if($this->uri->uri_string() == 'mahasiswa/krs') { echo 'active'; } ?>">Input KRS Online</a></li> 
-        <li><a href="<?php echo site_url("auth");?>" ">Logout</a></li>      
+        <!-- <li><a href="<?php echo site_url("mahasiswa/krs");?>" class="<?php if($this->uri->uri_string() == 'mahasiswa/krs') { echo 'active'; } ?>">Input KRS Online</a></li>
+        <li><a href="<?php echo site_url("mahasiswa/khs");?>" class="<?php if($this->uri->uri_string() == 'mahasiswa/krs') { echo 'active'; } ?>">Lihat KHS</a></li>   -->
+        <li style="float:right" class="acc-menu">
+        	<a href="javascript:;">Account</a>
+			<ul>
+				<li><a href="#">My Account</a></li>
+				<li><a href="<?php echo site_url("auth");?>">Logout</a></li>
+			</ul>
+        </li>      
       </ul>
     </div>
 
